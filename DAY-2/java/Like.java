64bc3e47bd80cba61985f6dff806f7eaa3924c72
York lin <superj80820@gmail.com>
2021-09-16 01:38:26 +0800

    update

--- DAY-2/java/Like.java
+++
@@ -1,8 +0,0 @@
-public class Like {
-    private int count = 0;
-
-    public synchronized void add() {
-        this.count++;
-        System.out.println("like count: " + this.count);
-    }
-}
--- DAY-2/java/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Like like = new Like();
-        new People(like).start();
-        new People(like).start();
-        new People(like).start();
-    }
-}

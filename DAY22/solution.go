512b2a6e09c772029bf9dcefbd2b115ad242aff5
York lin <superj80820@gmail.com>
2021-10-06 01:04:46 +0800

    update

--- DAY22/solution.go
+++ DAY22/solution.go
@@ -6,24 +6,34 @@ type PS5 interface {
 	StartGPUEngine()
 }
 
-type PS5Machine struct{}
+type PS5WithCD struct{}
 
-func (p PS5Machine) StartGPUEngine() {
+func (p PS5WithCD) StartGPUEngine() {
+	fmt.Println("start normal gpu engine")
+}
+
+type PS5WithDigital struct{}
+
+func (p PS5WithDigital) StartGPUEngine() {
 	fmt.Println("start normal gpu engine")
 }
 
 type PS5MachinePlus struct {
-	ps5Machine PS5Machine
+	ps5Machine PS5
+}
+
+func (p *PS5MachinePlus) SetPS5Machine(ps5 PS5) {
+	p.ps5Machine = ps5
 }
 
 func (p PS5MachinePlus) StartGPUEngine() {
 	p.ps5Machine.StartGPUEngine()
-	fmt.Println("start plus gpu engine")
+	fmt.Println("start plus plugin")
 }
 
 func main() {
 	ps5MachinePlus := PS5MachinePlus{
-		ps5Machine: PS5Machine{},
+		ps5Machine: PS5WithCD{}, // or PS5WithDigital{}
 	}
 	ps5MachinePlus.StartGPUEngine()
 }
